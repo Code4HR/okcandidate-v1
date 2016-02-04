fd3bdddc80a0cc4cd206ea34a8361125a0d30551
Blaine Price <1wbprice@gmail.com>
2016-02-03 23:04:46 -0500

    Fix trailing space, give mocha an empty test file to work with.

--- config/main.js
+++ config/main.js
@@ -24,6 +24,6 @@ module.exports = {
    * Define application paths here. "root" is the only required path.
    */
   paths: {
-    root: path.resolve(__dirname, '..') 
+    root: path.resolve(__dirname, '..')
   }
 }
