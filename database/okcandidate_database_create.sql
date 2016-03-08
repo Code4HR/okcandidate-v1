15b360046d9acfe9174944215350e6adb2fad599
ttavenner <tommy.tavenner@gmail.com>
2016-03-07 23:10:40 -0500

    add database creation and sample data scripts

---
+++ database/okcandidate_database_create.sql
@@ -0,0 +1,76 @@
+﻿--CREATE ROLE survey_manager LOGIN ENCRYPTED PASSWORD 'md52e5db3de6105661d19e5f4dd2e7b67cc' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+--CREATE DATABASE okcandidate WITH ENCODING='UTF8' CONNECTION LIMIT=-1;
+
+DROP TABLE IF EXISTS survey_answer;
+DROP TABLE IF EXISTS survey_response;
+DROP TABLE IF EXISTS answer;
+DROP TABLE IF EXISTS question;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS data_type;
+DROP TABLE IF EXISTS survey;
+DROP TABLE IF EXISTS congressional_district;
+DROP TABLE IF EXISTS response_type;
+DROP TABLE IF EXISTS users;
+
+-- CREATE TABLE IF NOT EXISTS users (
+-- 	id SERIAL PRIMARY KEY,
+-- 	user_name char(50) NOT NULL,
+-- 	user_password char(50) NOT NULL
+-- );
+-- 
+-- CREATE TABLE IF NOT EXISTS response_type (
+-- 	id SERIAL PRIMARY KEY,
+-- 	response_type char(100) NOT NULL
+-- );
+-- 
+-- CREATE TABLE IF NOT EXISTS congressional_district (
+-- 	id SERIAL PRIMARY KEY,
+-- 	district char(100) 
+-- );
+-- 
+-- CREATE TABLE IF NOT EXISTS survey (
+-- 	id SERIAL PRIMARY KEY,
+-- 	survey_name char(255) NOT NULL
+-- );
+-- 
+-- CREATE TABLE IF NOT EXISTS category (
+-- 	id SERIAL PRIMARY KEY,
+-- 	category_name char(100) NOT NULL
+-- );
+-- 
+-- CREATE TABLE IF NOT EXISTS data_type (
+-- 	id SERIAL PRIMARY KEY,
+-- 	type_name char(100) NOT NULL
+-- );
+-- 
+-- CREATE TABLE IF NOT EXISTS question (
+-- 	id SERIAL PRIMARY KEY,
+-- 	survey_id int NOT NULL REFERENCES survey,
+-- 	category_id int REFERENCES category,
+-- 	data_type int REFERENCES data_type,
+-- 	question_text char(255) NOT NULL
+-- );
+-- 
+-- CREATE TABLE IF NOT EXISTS answer (
+-- 	id SERIAL PRIMARY KEY,
+-- 	question_id int NOT NULL REFERENCES question,
+-- 	answer_label char(255) NOT NULL,
+-- 	answer_value char(255) NOT NULL,
+-- 	answer_order int NOT NULL
+-- );
+-- 
+-- CREATE TABLE IF NOT EXISTS survey_response (
+-- 	id SERIAL PRIMARY KEY,
+-- 	survey_id int NOT NULL REFERENCES survey,
+-- 	response_type int NOT NULL REFERENCES response_type,
+-- 	congressional_district int NOT NULL REFERENCES congressional_district,
+-- 	score numeric(10, 6) NOT NULL
+-- );
+-- 
+-- CREATE TABLE IF NOT EXISTS survey_answer (
+-- 	id SERIAL PRIMARY KEY,
+-- 	survey_response_id int NOT NULL REFERENCES survey_response,
+-- 	question_id int NOT NULL REFERENCES question,
+-- 	answer_id int NOT NULL REFERENCES answer,
+-- 	score numeric(10, 6) NOT NULL
+-- );
---
+++ database/okcandidate_database_sampledata.sql
@@ -0,0 +1,74 @@
+﻿-------------------
+-- Response Type --
+-------------------
+INSERT INTO response_type (response_type)
+VALUES ('Candidate'), ('Citizen');
+
+------------
+-- Survey --
+------------
+INSERT INTO survey (survey_name)
+VALUES ('2016 Norfolk Mayor & Council Race');
+
+--------------
+-- Category --
+--------------
+INSERT INTO category (category_name)
+VALUES ('Education'), ('Environment'), ('Public Works'), ('Revenue'), ('Economic Development'),
+('Tourism'), ('Public Safety'), ('Health and Human Services'), ('Arts, Entertainment, and Culture');
+
+---------------
+-- Data Type --
+---------------
+INSERT INTO data_type (type_name)
+VALUES ('Multiple Choice'), ('Scale');
+
+--------------
+-- Question --
+--------------
+INSERT INTO question (survey_id, category_id, data_type, question_text) VALUES
+((SELECT id FROM survey WHERE survey_name = '2016 Norfolk Mayor & Council Race'), 
+ (SELECT id FROM category WHERE category_name = 'Education'),
+ (Select id FROM data_type WHERE type_name = 'Multiple Choice'),
+ 'Which of the following best describes your attitude toward K-12 education in your city?'),
+((SELECT id FROM survey WHERE survey_name = '2016 Norfolk Mayor & Council Race'), 
+ (SELECT id FROM category WHERE category_name = 'Education'),
+ (Select id FROM data_type WHERE type_name = 'Scale'),
+ 'How important is K-12 education in your city?');
+
+------------
+-- Answer --
+------------
+INSERT INTO answer (question_id, answer_label, answer_value, answer_order) VALUES
+((SELECT id FROM question WHERE question_text = 'Which of the following best describes your attitude toward K-12 education in your city?'),
+ 'We need to overhaul and reinvest in our public K-12 system',
+ 1,
+ 1),
+ ((SELECT id FROM question WHERE question_text = 'Which of the following best describes your attitude toward K-12 education in your city?'),
+ 'We need to change our approach to K-12 education in support of privately funded alternatives',
+ 2,
+ 2),
+ ((SELECT id FROM question WHERE question_text = 'Which of the following best describes your attitude toward K-12 education in your city?'),
+ 'We need a comprehensive approach that boosts our public system while introducing alternative approaches',
+ 3,
+ 3),
+((SELECT id FROM question WHERE question_text = 'How important is K-12 education in your city?'),
+ 'Not Important',
+ 1,
+ 1),
+((SELECT id FROM question WHERE question_text = 'How important is K-12 education in your city?'),
+ '2',
+ 2,
+ 2),
+((SELECT id FROM question WHERE question_text = 'How important is K-12 education in your city?'),
+ '3',
+ 3,
+ 3),
+((SELECT id FROM question WHERE question_text = 'How important is K-12 education in your city?'),
+ '4',
+ 4,
+ 4),
+((SELECT id FROM question WHERE question_text = 'How important is K-12 education in your city?'),
+ 'Important',
+ 5,
+ 5);
