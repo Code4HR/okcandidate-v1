867133bf33ecd065babb45dbad9243fbc1ba5a42
Stanley Zheng <szheng@thisisgrow.com>
2016-03-20 18:26:42 -0400

    flattening controller handlers

--- api/routes.js
+++ api/routes.js
@@ -4,13 +4,13 @@ module.exports = (server) => {
 
 
   const API_ROUTES = [].concat(
-     require('./controllers/category/')(server),
-     require('./controllers/DataType/')(server),
-     require('./controllers/Survey/')(server),
-     require('./controllers/Question/')(server),
-     require('./controllers/Answer/')(server),
-     require('./controllers/SurveyResponse/')(server),
-     require('./controllers/SurveyAnswer/')(server)
+     require('./controllers/Category')(server),
+     require('./controllers/DataType')(server),
+     require('./controllers/Survey')(server),
+     require('./controllers/Question')(server),
+     require('./controllers/Answer')(server),
+     require('./controllers/SurveyResponse')(server),
+     require('./controllers/SurveyAnswer')(server)
   )
 
   return [
