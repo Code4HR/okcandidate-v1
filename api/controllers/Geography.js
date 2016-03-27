b6be7b473b5fb0c469f9a8119826025497fb0926
Stanley Zheng <dev.stanzheng@gmail.com>
2016-03-26 21:43:47 -0400

    adding reverse ward geocoding

--- api/controllers/Geography.js
+++ api/controllers/Geography.js
@@ -1,6 +1,9 @@
 'use strict'
 require('isomorphic-fetch')
 
+var turf = require('turf')
+var superwards = turf.featurecollection(require('../data/superwards.json'))
+
 const SMARTY_STREETS_API = 'https://api.smartystreets.com/street-address'
 const SMART_STREET_AUTH_ID = process.env['SS_AUTH_ID']
 const SMART_STREET_AUTH_TOKEN = process.env['SS_AUTH_TOKEN']
@@ -28,8 +31,12 @@ module.exports = function (server) {
         fetch(url)
           .then(function (response) {
             // TODO HANDLE HAPI ERROR
-            reply(response.json())
+            return response.json()
           })
+          .then(function (address) {
+            return whichWard(address)
+          })
+          .then(reply)
       }
     }]
 }
@@ -42,11 +49,20 @@ function buildStreetQuery (street1) {
     {'key': 'street', 'value': street1.replace(/ /g, '+')},
     {'key': 'city', 'value': 'norfolk'},
     {'key': 'state', 'value': 'VA'},
-    {'key': 'candidates', 'value': 10}
+    {'key': 'candidates', 'value': 1}
   ]
   let query = ''
   params.forEach((p) => {
     query = `${query}&${p.key}=${p.value}`
   })
   return `${SMARTY_STREETS_API}?${query.slice(1)}`
+}
+
+function whichWard (address) {
+  // There are only two feature collections/wards so we can guess if its not 6, its 7
+  if (address.length === 0) {
+    return {'error': 'address not found'}
+  }
+  let point = turf.point([address[0].metadata.longitude, address[0].metadata.latitude])
+  return turf.inside(point, superwards.features.features[0]) ? {'ward': 6} : {'ward': 7}
 }
