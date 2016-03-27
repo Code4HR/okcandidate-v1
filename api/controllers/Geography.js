65a39c0c0342c7a97c03c0c1b8944b1d11a07a3e
Stanley Zheng <dev.stanzheng@gmail.com>
2016-03-26 21:18:52 -0400

    adding smarty streets ward geocode

--- api/controllers/Geography.js
+++ api/controllers/Geography.js
@@ -1,4 +1,9 @@
 'use strict'
+require('isomorphic-fetch')
+
+const SMARTY_STREETS_API = 'https://api.smartystreets.com/street-address'
+const SMART_STREET_AUTH_ID = process.env['SS_AUTH_ID']
+const SMART_STREET_AUTH_TOKEN = process.env['SS_AUTH_TOKEN']
 
 module.exports = function (server) {
   const Geography = server.plugins['hapi-shelf'].model('Geography')
@@ -12,7 +17,36 @@ module.exports = function (server) {
         .then(geographies => {
           reply(geographies)
         })
-
     }
-  }]
+  },
+    {
+      method: 'POST',
+      path: '/api/geography/ward',
+      handler: (request, reply) => {
+        let url = buildStreetQuery(request.payload.street)
+        console.log(url)
+        fetch(url)
+          .then(function (response) {
+            // TODO HANDLE HAPI ERROR
+            reply(response.json())
+          })
+      }
+    }]
+}
+
+function buildStreetQuery (street1) {
+  // / Builds a GET query string
+  let params = [
+    {'key': 'auth-id', 'value': SMART_STREET_AUTH_ID},
+    {'key': 'auth-token', 'value': SMART_STREET_AUTH_TOKEN},
+    {'key': 'street', 'value': street1.replace(/ /g, '+')},
+    {'key': 'city', 'value': 'norfolk'},
+    {'key': 'state', 'value': 'VA'},
+    {'key': 'candidates', 'value': 10}
+  ]
+  let query = ''
+  params.forEach((p) => {
+    query = `${query}&${p.key}=${p.value}`
+  })
+  return `${SMARTY_STREETS_API}?${query.slice(1)}`
 }
