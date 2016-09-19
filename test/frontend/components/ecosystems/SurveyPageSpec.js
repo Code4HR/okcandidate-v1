bc016c1b97e62c38629d1c70d88ace8cb10bbb47
Blaine Price <1wbprice@gmail.com>
2016-09-19 12:25:03 -0400

    Add categories fixture file.  Use on SurveyPageSpec and SurveyQuestionPagerTest.js

--- test/frontend/components/ecosystems/SurveyPageSpec.js
+++ test/frontend/components/ecosystems/SurveyPageSpec.js
@@ -4,6 +4,9 @@ import { browserHistory } from 'react-router'
 import { expect } from 'chai'
 import sinon from 'sinon'
 
+import questions from './../fixtures/questions'
+import categories from './../fixtures/categories'
+
 import SurveyPage from '../../../../assets/js/components/ecosystems/SurveyPage'
 import LoadingIndicator
   from '../../../../assets/js/components/atoms/LoadingIndicator'
@@ -22,112 +25,8 @@ describe('The survey page component', () => {
     }
     state.returns({
       survey: {
-        categories: [
-          {
-            categoryName: 'Transportation',
-            firstQuestionIndex: 0,
-            id: 1,
-            surveyId: 1
-          },
-          {
-            categoryName: 'Economics',
-            firstQuestionIndex: 3,
-            id: 2,
-            surveyId: 1
-          },
-          {
-            categoryName: 'Governance',
-            firstQuestionIndex: 6,
-            id: 3,
-            surveyId: 1
-          }
-        ],
-        questions: [
-          {
-            'id': 1,
-            'surveyId': 1,
-            'categoryId': 1,
-            'questionText': 'Do you support the city\’s Complete Streets initiative?',
-            'answers': [
-              {
-                'id': 1,
-                'questionId': 1,
-                'answerLabel': 'Yes, streets should accommodate bikes and pedestrians in addition to cars.',
-                'answerValue': 'Yes, streets should accommodate bikes and pedestrians in addition to cars.'
-              },
-              {
-                'id': 2,
-                'questionId': 1,
-                'answerLabel': 'To a degree.',
-                'answerValue': 'To a degree.'
-              },
-              {
-                'id': 3,
-                'questionId': 1,
-                'answerLabel': 'No. Our city was designed for vehicular traffic and should stay that way.',
-                'answerValue': 'No. Our city was designed for vehicular traffic and should stay that way.'
-              }
-            ]
-          },
-          {
-            'id': 2,
-            'surveyId': 1,
-            'categoryId': 1,
-            'questionText': 'Was The Tide a good use of city money?',
-            'answers': [
-              {
-                'id': 4,
-                'questionId': 2,
-                'answerLabel': 'Absolutely. The Tide showed visionary leadership.',
-                'answerValue': 'Absolutely. The Tide showed visionary leadership.'
-              },
-              {
-                'id': 5,
-                'questionId': 2,
-                'answerLabel': 'It remains to be seen.',
-                'answerValue': 'It remains to be seen.'
-              },
-              {
-                'id': 6,
-                'questionId': 2,
-                'answerLabel': 'No. This was inefficient transportation spending.',
-                'answerValue': 'No. This was inefficient transportation spending.'
-              }
-            ]
-          },
-          {
-            'id': 3,
-            'surveyId': 1,
-            'categoryId': 1,
-            'questionText': 'Where should The Tide go next?',
-            'answers': [
-              {
-                'id': 7,
-                'questionId': 3,
-                'answerLabel': 'ODU',
-                'answerValue': 'ODU'
-              },
-              {
-                'id': 8,
-                'questionId': 3,
-                'answerLabel': 'Norfolk International Airport',
-                'answerValue': 'Norfolk International Airport'
-              },
-              {
-                'id': 9,
-                'questionId': 3,
-                'answerLabel': 'Naval Station Norfolk',
-                'answerValue': 'Naval Station Norfolk'
-              },
-              {
-                'id': 10,
-                'questionId': 3,
-                'answerLabel': 'Nowhere',
-                'answerValue': 'Nowhere'
-              }
-            ]
-          }
-        ],
+        categories: categories,
+        questions: questions,
         responses: [],
         isFetching: false
       }
