d084afc1b034904aa88844fa2ba43d6edcfb0ac8
Stanley Zheng <dev.stanzheng@gmail.com>
2016-10-14 18:24:38 -0400

    adding OG data template, populate by return state query

--- views/Default.jsx
+++ views/Default.jsx
@@ -4,12 +4,44 @@ const React = require('react');
 
 const Default = React.createClass({
 
+  getInitialState: function() {
+    return {
+        tags: [
+        {name: "description", content: "lorem ipsum dolor"},
+        {itemProp: "name", content: "The Name or Title Here"},
+        {itemProp: "description", content: "This is the page description"},
+        {itemProp: "image", content: "http://www.example.com/image.jpg"},
+        {name: "twitter:card", content: "product"},
+        {name: "twitter:site", content: "@publisher_handle"},
+        {name: "twitter:title", content: "Page Title"},
+        {name: "twitter:description", content: "Page description less than 200 characters"},
+        {name: "twitter:creator", content: "@author_handle"},
+        {name: "twitter:image", content: "http://www.example.com/image.html"},
+        {name: "twitter:data1", content: "$3"},
+        {name: "twitter:label1", content: "Price"},
+        {name: "twitter:data2", content: "Black"},
+        {name: "twitter:label2", content: "Color"},
+        {property: "og:title", content: "Title Here"},
+        {property: "og:type", content: "article"},
+        {property: "og:url", content: "http://www.example.com/"},
+        {property: "og:image", content: "http://example.com/image.jpg"},
+        {property: "og:description", content: "Description Here"},
+        {property: "og:site_name", content: "Site Name, i.e. Moz"},
+        {property: "og:price:amount", content: "15.00"},
+        {property: "og:price:currency", content: "USD"},
+    ]}
+  },
+
   render: function() {
 
     return(
       <html>
         <head>
           <meta charSet="utf-8"></meta>
+          {
+            this.state.tags.map((tag, index) =>
+              <meta data-doc-meta="true" key={index} {...tag} />)
+          }
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
           <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet"></link>
           <title>OK Candidate</title>
